ebf5d718e43b17bb41cb9f6f4a25631761a03bce
Akshat Mani Tripathi <iamak2407@gmail.com>
2026-03-17 02:11:08 +0530

    doing upload yet

--- file_upload_system/src/middleware/uploadMiddleware.js
+++ file_upload_system/src/middleware/uploadMiddleware.js
@@ -1,1 +1,20 @@
-const multer= require('multer');
+const multer = require("multer");
+const fileHelpers = require("../utils/fileHelpers");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadPath = fileHelpers.getUserUploadPath(req.user);
+        cb(null, uploadPath);
+    },
+
+    filename: (req, file, cb) => {
+        const filename = fileHelpers.randomFileName(file.originalname);
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage });
+
+const uploadMiddleware = upload.single("file");
+
+module.exports = uploadMiddleware;
